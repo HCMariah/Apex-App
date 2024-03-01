2de5a466fccbff661c5a9b9d3e95156d6378e553
HCMariah <michaletmedina@outlook.com>
2024-03-01 00:05:10 -0700

    Edited the File path for assets out_path

--- ApexTeamBuilderv1.1/Apex_Teams.py
+++ ApexTeamBuilderv1.1/Apex_Teams.py
@@ -11,7 +11,7 @@ from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
 
 
 OUTPUT_PATH = Path(__file__).parent
-ASSETS_PATH = OUTPUT_PATH / Path(r"C:\Users\Micha\OneDrive\Documents\VSCodingProjects\Apex Teams GUI\build\assets\frame0")
+ASSETS_PATH = OUTPUT_PATH / Path(r".\assets")
 
 
 def relative_to_assets(path: str) -> Path:
